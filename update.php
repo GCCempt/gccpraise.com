9a8612ea6bbaf46782ff098cbcef0d7439057f44
infosecguy10 <infosecguy10@gmail.com>
2021-12-18 02:03:59 -0500

    include sets in update script

--- update.php
+++ update.php
@@ -1,14 +1,10 @@
 <?php
 $dropbox_copy_dir = 'dropbox_copy/';
-$song_directory = "www/xml/";
+$song_directory = "songs/";
 
 
 if (!is_dir($dropbox_copy_dir)) {
     mkdir($dropbox_copy_dir . "/Songs/");
-
-}
-if (!is_dir("www/xml/")) {
-    mkdir($song_directory);
 
 }
 
@@ -37,7 +33,45 @@ if ($res === TRUE) {
 $files = scandir($dropbox_copy_dir . "/Songs/");
 foreach ($files as $filename) {
     if (!is_dir($filename)) {
-        copy($dropbox_copy_dir . "Songs/" . $filename, "www/xml/" . $filename);
+        copy($dropbox_copy_dir . "Songs/" . $filename, "songs/" . $filename);
     }
     header("Location: preview_index.php");
+}
+
+// Read DropBox Sets folder and store in web directory
+$sets_dropbox_copy_dir = 'dropbox_copy/Sets/';
+$sets_directory = "sets/";
+if (!is_dir($sets_dropbox_copy_dir)) {
+    mkdir($sets_dropbox_copy_dir);
+
+}
+
+$env = getenv('DROPBOX_TOKEN');
+$url = "https://content.dropboxapi.com/2/files/download_zip";
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$headers = array('Accept: text/plain', 'Content-Type: text/plain', 'Authorization: Bearer ' . $env, 'Dropbox-API-Arg: {"path":"/OpenSongV2/OpenSong Data/Sets/"}');
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+$resp = curl_exec($curl);
+curl_close($curl);
+$filename = 'Sets.zip';
+$sets = fopen($filename, 'w');
+fwrite($sets, $resp);
+$zip = new ZipArchive;
+$res = $zip->open($filename);
+if ($res === TRUE) {
+    $zip->extractTo($sets_dropbox_copy_dir);
+    $zip->close();
+    echo "Success!";
+} else {
+    echo 'Error!';
+}
+$files = scandir($sets_dropbox_copy_dir);
+foreach ($files as $filename) {
+    if (!is_dir($filename)) {
+        copy($sets_dropbox_copy_dir . $filename, "sets/" . $filename);
+    }
+    header("Location: preview_set.php");
 }
