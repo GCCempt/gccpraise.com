15ab47fd0b6bcb1c5d57e60106d62f47cffe81d6
infosecguy10 <infosecguy10@gmail.com>
2021-12-17 23:01:47 -0500

    move live and bulletin to root

--- www/index.php
+++
@@ -1,9 +0,0 @@
-     <!DOCTYPE html>  
-     <head>  
-      <title>Hello World!</title>
-     </head>  
-
-     <body>  
-      <h1>Hello Steve's World!</h1>
-      <p><?php echo 'We are running PHP, version: ' . phpversion(); ?></p>
-     </body>
